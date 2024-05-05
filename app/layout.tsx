86233aac9bdb761159f90e95796fa642985b99ec
Jose Sanchez <jsanxez7@gmail.com>
2024-05-05 15:19:29 -0500

    chore(structure): relocates stylesheet and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { roboto } from "@/app/ui/fonts";
-import "./globals.css";
+import "@/app/ui/globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",

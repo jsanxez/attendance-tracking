be04b21dc71067068f6b8e3351670df1aafb1f37
Jose Sanchez <jsanxez7@gmail.com>
2024-05-12 18:30:14 -0500

    style(overview): reduce gap size

--- app/ui/overview.tsx
+++ app/ui/overview.tsx
@@ -4,14 +4,14 @@ export function Overview({ title, children }) {
     return (
         <div className="w-fit text-center bg-surface-variant rounded-xl p-4">
             <h2 className='mb-6 text-xl'>{title}</h2>
-            <div className='inline-flex gap-6'>{children}</div>
+            <div className='inline-flex gap-4'>{children}</div>
         </div>
     )
 }
 
 export function OverviewItem({ iconName, label, counter, isError=false }) {
     return (
-        <div className="inline-flex items-center gap-6 p-4 pl-6 bg-surface rounded-xl">
+        <div className="inline-flex items-center gap-2 p-4 pl-6 bg-surface rounded-xl">
             <Icon iconName={iconName} isError={isError} />  
             <div className="text-right text-on-surface">
                 <p className="text-xl">{counter}</p>

82aba47516c1ca6e82671da3ff5e2d0e64dbb233
Jose Sanchez <jsanxez7@gmail.com>
2024-05-23 23:11:57 -0500

    style(overview): set color to show properly in firefox

--- app/ui/overview.tsx
+++ app/ui/overview.tsx
@@ -3,7 +3,7 @@ import { symbols } from './fonts'
 export function Overview({ title, children }) {
     return (
         <div className="w-fit text-center bg-surface-variant rounded-xl p-4">
-            <h2 className='mb-6 text-xl'>{title}</h2>
+            <h2 className='mb-6 text-xl text-on-surface'>{title}</h2>
             <div className='inline-flex gap-4'>{children}</div>
         </div>
     )
